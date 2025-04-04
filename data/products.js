96aecf44498dee298917341635691f7040a25c5c
Kerolos <44545152+KerolosGergs@users.noreply.github.com>
2025-04-04 12:06:07 +0200

    make cards dynamic

--- data/products.js
+++ data/products.js
@@ -11,34 +11,51 @@ let products ={
 };
 
 async function fetchCategory(){
-  const now = Date.now();
-  const expiryTime = 5 * 60 * 1000; // 5 minutes
-  if (categories.data && now - categories.timestamp < expiryTime) {
-    return categories.data;
-  }
-  try{
-  let response = await fetch('https://dummyjson.com/products/categories');
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
-  }
-  let json = await response.json();
-  categories= {
-    data: json,
-    timestamp: now
+  // const now = Date.now();
+  // const expiryTime = 5 * 60 * 1000; // 5 minutes
+  // if (categories.data && now - categories.timestamp < expiryTime) {
+  //   return categories.data;
+  // }
+  // try{
+  // let response = await fetch('https://dummyjson.com/products/categories');
+  // if (!response.ok) {
+  //   throw new Error(`Response status: ${response.status}`);
+  // }
+  // let json = await response.json();
+  // categories= {
+  //   data: json,
+  //   timestamp: now
 
-  }
-  return json;
-  }catch(er){
-    console.log(er);
-  }
+  // }
+  // return json;
+  // }catch(er){
+  //   console.log(er);
+  // }
+  const now = Date.now();
+  let groupByCategory = await fetchProducts();
+   groupByCategory = groupByCategory.reduce((acc, product) => {
+    if (!acc[product.category]) {
+      acc[product.category] = [];
+    }
+    acc[product.category].push(product);
+    return acc;
+  }, {});
+    categories = {
+      data:Object.keys(groupByCategory),
+      timestamp:now
+    }
+  return Object.keys(groupByCategory);
+  // groupByCategory = groupByCategory.filter(element=>element.length>0);
+  // console.log(groupByCategory);
 
 }
 
 
 
 export async function getCategory() {
-  
+      
       let json = await fetchCategory();
+      console.log(json);
       json.forEach(element => {
         addCategoryBtn(element);
       });
@@ -47,7 +64,7 @@ export async function getCategory() {
     
 
 
-async function fetchProducts(){
+export async function fetchProducts(){
   const now = Date.now();
   const expiryTime = 5 * 60 * 1000; // 5 minutes
   if (products.data && now - products.timestamp < expiryTime) {
